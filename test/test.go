880cba3c7f5c7bfc419a57a3f9af4baa841c20a0
Philipp Gille <philipp.gille@gmail.com>
2018-11-08 22:50:54 +0100

    Add test method for testing more types
    - Previously only a struct was tested

--- test/test.go
+++ test/test.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/go-test/deep"
+
 	"github.com/philippgille/gokv"
 )
 
@@ -14,7 +16,8 @@ type Foo struct {
 	Bar string
 }
 
-// TestStore tests if reading from and writing to the store works properly.
+// TestStore tests if reading from, writing to and deleting from the store works properly.
+// A struct is used as value. See TestTypes() for a test that is simpler but tests all types.
 func TestStore(store gokv.Store, t *testing.T) {
 	key := strconv.FormatInt(rand.Int63(), 10)
 
@@ -50,7 +53,7 @@ func TestStore(store gokv.Store, t *testing.T) {
 		t.Error(err)
 	}
 	if !found {
-		t.Errorf("No value was found, but should have been")
+		t.Error("No value was found, but should have been")
 	}
 	actual := *actualPtr
 	if actual != expected {
@@ -69,6 +72,128 @@ func TestStore(store gokv.Store, t *testing.T) {
 	}
 	if found {
 		t.Error("A value was found, but no value was expected")
+	}
+}
+
+// TestTypes tests if setting and getting values works with all Go types.
+func TestTypes(store gokv.Store, t *testing.T) {
+	b := true
+	f := 1.2
+	i := 1
+	r := '⚡'
+	s := "foo"
+
+	ba := []bool{true, false}
+	byteA := []byte("foo")
+	ia := []int{1, 2}
+	sa := []string{"foo", "bar"}
+
+	testVals := []struct {
+		subTestName string
+		val         interface{}
+		testGet     func(*testing.T, gokv.Store, string, interface{})
+	}{
+		{"bool", b, func(t *testing.T, store gokv.Store, key string, expected interface{}) {
+			actualPtr := new(bool)
+			found, err := store.Get(key, actualPtr)
+			handleGetError(t, err, found)
+			actual := *actualPtr
+			if actual != expected {
+				t.Errorf("Expected: %v, but was: %v", expected, actual)
+			}
+		}},
+		{"float", f, func(t *testing.T, store gokv.Store, key string, expected interface{}) {
+			actualPtr := new(float64)
+			found, err := store.Get(key, actualPtr)
+			handleGetError(t, err, found)
+			actual := *actualPtr
+			if actual != expected {
+				t.Errorf("Expected: %v, but was: %v", expected, actual)
+			}
+		}},
+		{"int", i, func(t *testing.T, store gokv.Store, key string, expected interface{}) {
+			actualPtr := new(int)
+			found, err := store.Get(key, actualPtr)
+			handleGetError(t, err, found)
+			actual := *actualPtr
+			if actual != expected {
+				t.Errorf("Expected: %v, but was: %v", expected, actual)
+			}
+		}},
+		{"rune", r, func(t *testing.T, store gokv.Store, key string, expected interface{}) {
+			actualPtr := new(rune)
+			found, err := store.Get(key, actualPtr)
+			handleGetError(t, err, found)
+			actual := *actualPtr
+			if actual != expected {
+				t.Errorf("Expected: %v, but was: %v", expected, actual)
+			}
+		}},
+		{"string", s, func(t *testing.T, store gokv.Store, key string, expected interface{}) {
+			actualPtr := new(string)
+			found, err := store.Get(key, actualPtr)
+			handleGetError(t, err, found)
+			actual := *actualPtr
+			if actual != expected {
+				t.Errorf("Expected: %v, but was: %v", expected, actual)
+			}
+		}},
+		{"slice of bool", ba, func(t *testing.T, store gokv.Store, key string, expected interface{}) {
+			actualPtr := new([]bool)
+			found, err := store.Get(key, actualPtr)
+			handleGetError(t, err, found)
+			actual := *actualPtr
+			if diff := deep.Equal(actual, expected); diff != nil {
+				t.Error(diff)
+			}
+		}},
+		{"slice of byte", byteA, func(t *testing.T, store gokv.Store, key string, expected interface{}) {
+			actualPtr := new([]byte)
+			found, err := store.Get(key, actualPtr)
+			handleGetError(t, err, found)
+			actual := *actualPtr
+			if diff := deep.Equal(actual, expected); diff != nil {
+				t.Error(diff)
+			}
+		}},
+		{"slice of int", ia, func(t *testing.T, store gokv.Store, key string, expected interface{}) {
+			actualPtr := new([]int)
+			found, err := store.Get(key, actualPtr)
+			handleGetError(t, err, found)
+			actual := *actualPtr
+			if diff := deep.Equal(actual, expected); diff != nil {
+				t.Error(diff)
+			}
+		}},
+		{"slice of string", sa, func(t *testing.T, store gokv.Store, key string, expected interface{}) {
+			actualPtr := new([]string)
+			found, err := store.Get(key, actualPtr)
+			handleGetError(t, err, found)
+			actual := *actualPtr
+			if diff := deep.Equal(actual, expected); diff != nil {
+				t.Error(diff)
+			}
+		}},
+	}
+
+	for _, testVal := range testVals {
+		t.Run(testVal.subTestName, func(t2 *testing.T) {
+			key := strconv.FormatInt(rand.Int63(), 10)
+			err := store.Set(key, testVal.val)
+			if err != nil {
+				t.Error(err)
+			}
+			testVal.testGet(t, store, key, testVal.val)
+		})
+	}
+}
+
+func handleGetError(t *testing.T, err error, found bool) {
+	if err != nil {
+		t.Error(err)
+	}
+	if !found {
+		t.Error("No value was found, but should have been")
 	}
 }
 
