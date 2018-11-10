b4e8c5e0c4bc40076672da9071a83eafc84a9b8a
Philipp Gille <philipp.gille@gmail.com>
2018-11-10 18:16:44 +0100

    Add gob (un-)marshalling methods to util package

--- util/util.go
+++ util/util.go
@@ -1,6 +1,10 @@
 package util
 
-import "encoding/json"
+import (
+	"bytes"
+	"encoding/gob"
+	"encoding/json"
+)
 
 // ToJSON marshals the given value into JSON.
 func ToJSON(v interface{}) ([]byte, error) {
@@ -10,4 +14,22 @@ func ToJSON(v interface{}) ([]byte, error) {
 // FromJSON unmarshals the given JSON and populates the value that the given pointer points to accordingly.
 func FromJSON(data []byte, v interface{}) error {
 	return json.Unmarshal(data, v)
+}
+
+// ToGob marshals the given value into a gob.
+func ToGob(v interface{}) ([]byte, error) {
+	buffer := new(bytes.Buffer)
+	encoder := gob.NewEncoder(buffer)
+	err := encoder.Encode(v)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
+}
+
+// FromGob unmarshals the given gob and populates the value that the given pointer points to accordingly.
+func FromGob(data []byte, v interface{}) error {
+	reader := bytes.NewReader(data)
+	decoder := gob.NewDecoder(reader)
+	return decoder.Decode(v)
 }
