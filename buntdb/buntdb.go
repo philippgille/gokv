c768b339cde709892212b3b032aabe25c72569fb
tdakkota <tanc13@yandex.ru>
2020-02-11 14:46:43 +0300

    Add more options

--- buntdb/buntdb.go
+++ buntdb/buntdb.go
@@ -65,19 +65,55 @@ func (s Store) Close() error {
 	return s.db.Close()
 }
 
+type SyncPolicy = buntdb.SyncPolicy
+
+const (
+	Never       = buntdb.Never
+	EverySecond = buntdb.EverySecond
+	Always      = buntdb.Always
+)
+
 // Options are the options for the BuntDB instance.
 type Options struct {
 	// Path to database file.
 	// Optional (":memory:" by default).
 	Path string
+
+	// SyncPolicy adjusts how often the data is synced to disk.
+	// This value can be Never, EverySecond, or Always.
+	// Optional (EverySecond by default).
+	SyncPolicy SyncPolicy
+
+	// AutoShrinkPercentage is used by the background process to trigger
+	// a shrink of the aof file when the size of the file is larger than the
+	// percentage of the result of the previous shrunk file.
+	// For example, if this value is 100, and the last shrink process
+	// resulted in a 100mb file, then the new aof file must be 200mb before
+	// a shrink is triggered.
+	// Optional (100% by default).
+	AutoShrinkPercentage int
+
+	// AutoShrinkMinSize defines the minimum size of the aof file before
+	// an automatic shrink can occur.
+	// Optional (32KB by default).
+	AutoShrinkMinSize int
+
+	// AutoShrinkDisabled turns off automatic background shrinking
+	// Optional (false by default).
+	AutoShrinkDisabled bool
+
 	// Encoding format.
 	// Optional (encoding.JSON by default).
 	Codec encoding.Codec
 }
 
 var DefaultOptions = Options{
-	Path:  ":memory:",
-	Codec: encoding.JSON,
+	Path:                 ":memory:",
+	SyncPolicy:           EverySecond,
+	AutoShrinkPercentage: 100,
+	AutoShrinkMinSize:    32 * 1024 * 1024,
+	AutoShrinkDisabled:   false,
+	Codec:                encoding.JSON,
 }
 
 func NewStore(options Options) (Store, error) {
@@ -92,6 +128,16 @@ func NewStore(options Options) (Store, error) {
 	}
 
 	db, err := buntdb.Open(options.Path)
+	if err != nil {
+		return result, err
+	}
+
+	err = db.SetConfig(buntdb.Config{
+		SyncPolicy:           options.SyncPolicy,
+		AutoShrinkPercentage: options.AutoShrinkPercentage,
+		AutoShrinkMinSize:    options.AutoShrinkMinSize,
+		AutoShrinkDisabled:   options.AutoShrinkDisabled,
+	})
 	if err != nil {
 		return result, err
 	}
