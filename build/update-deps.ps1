050e78467c501a8a7b5236603f5ffda4cddcd7d7
Philipp Gillé <philipp.gille@gmail.com>
2022-05-07 23:10:55 +0200

    Improve dep update scripts

--- build/update-deps.ps1
+++ build/update-deps.ps1
@@ -1,21 +1,22 @@
 $workingDir = $(pwd).Path
 
-# Note: To update only the direct dependencies, use:
-# go get $(go list -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all)
+# We don't want to update transitive dependencies, so instead of using `go get -u` we use
+# `go get $(go list -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all)`
+# as suggested in https://github.com/golang/go/issues/28424#issuecomment-1101896499.
 
 # Helper packages
 $array = @("encoding","sql","test", "util")
 foreach ($moduleName in $array){
     echo "updating $moduleName"
-    cd "$PSScriptRoot/../$moduleName"; go get -u -t; go mod tidy; cd $workingDir
+    cd "$PSScriptRoot/../$moduleName"; go get $(go list -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all); go mod tidy; cd $workingDir
 }
 
 # Implementations
 cat "$PSScriptRoot/implementations" | foreach {
     echo "updating $_"
-    cd "$PSScriptRoot/../$_"; go get -u -t; go mod tidy; cd $workingDir
+    cd "$PSScriptRoot/../$_"; go get $(go list -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all); go mod tidy; cd $workingDir
 }
 
 # Examples
 echo "updating examples"
-cd "$PSScriptRoot/../examples"; go get -u -t; go mod tidy; cd $workingDir
+cd "$PSScriptRoot/../examples"; go get $(go list -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all); go mod tidy; cd $workingDir
--- build/update-deps.sh
+++ build/update-deps.sh
@@ -3,6 +3,9 @@
 # Updates all dependencies.
 # go-cloud is doing this in a similar way:
 # https://github.com/google/go-cloud/blob/be25177dcd0a4e5202ef7f32deef6fdb5261da00/internal/testing/update_deps.sh
+# But we don't want to update transitive dependencies, so instead of using `go get -u` we use
+# `go get $(go list -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all)`
+# as suggested in https://github.com/golang/go/issues/28424#issuecomment-1101896499.
 
 set -euxo pipefail
 
@@ -11,23 +14,20 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export GO111MODULE=on
 
-# Note: To update only the direct dependencies, use:
-# go get $(go list -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all)
-
 # Helper packages
 array=( encoding sql test util )
 for MODULE_NAME in "${array[@]}"; do
     echo "updating $MODULE_NAME"
-    (cd "$SCRIPT_DIR"/../"$MODULE_NAME" && go get -u -t && go mod tidy) || (cd "$WORKING_DIR" && echo " failed" && exit 1)
+    (cd "$SCRIPT_DIR"/../"$MODULE_NAME" && go get $(go list -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all) && go mod tidy) || (cd "$WORKING_DIR" && echo " failed" && exit 1)
 done
 
 # Implementations
 cat "$SCRIPT_DIR"/implementations | while read -r MODULE_NAME; do
     echo "updating $MODULE_NAME"
-    (cd "$SCRIPT_DIR"/../"$MODULE_NAME" && go get -u -t && go mod tidy) || (cd "$WORKING_DIR" && echo " failed" && exit 1)
+    (cd "$SCRIPT_DIR"/../"$MODULE_NAME" && go get $(go list -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all) && go mod tidy) || (cd "$WORKING_DIR" && echo " failed" && exit 1)
 done
 
 # Examples
-(cd "$SCRIPT_DIR"/../examples && go get -u -t && go mod tidy) || (cd "$WORKING_DIR" && echo "update failed" && exit 1)
+(cd "$SCRIPT_DIR"/../examples && go get $(go list -f '{{if not (or .Main .Indirect)}}{{.Path}}{{end}}' -m all) && go mod tidy) || (cd "$WORKING_DIR" && echo "update failed" && exit 1)
 
 cd "$WORKING_DIR"
